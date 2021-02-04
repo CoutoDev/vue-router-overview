42651d09ddcbebd84a3a8e1dbd1b7036f9ee874f
CoutoDev <c.svitor@yahoo.com.br>
2021-02-03 21:35:03 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Router Overview</h1>
+
+  <div class="div-router">
+    <button @click="goToPage('/pageA')">Go to Page A</button>
+    <button @click="goToPage('/pageb')">Go to Page b</button>
+    <button @click="goToPage('/pageC')">Go to Page C</button>
+  </div>
+
+  <div class="div-router">
+    <button @click="replacePage('/pageA')">Replace Page A</button>
+    <button @click="replacePage('/pageb')">Replace Page b</button>
+    <button @click="replacePage('/pageC')">Replace Page C</button>
+  </div>
+
+  <div class="div-router">
+    <button @click="goToUser">Go to User</button>
+  </div>
+
+  <div class="div-router">
+    <router-link to="/pageA">Page A</router-link>
+    <router-link to="/pageB">Page B</router-link>
+    <router-link to="/pageC">Page C</router-link>
+    <router-link :to="{name: 'user', params: {userId: 1}}">User</router-link>
+  </div>
+
+  <router-view></router-view>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  methods: {
+    goToPage(route) {
+      this.$router.push(route)
+    },
+    replacePage(route) {
+      this.$router.replace(route)
+    },
+    goToUser() {
+      this.$router.push({ name: "user", params: { userId: 2 } })
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .div-router {
+    display: flex;
+    justify-content: space-around;
+  }
 </style>
